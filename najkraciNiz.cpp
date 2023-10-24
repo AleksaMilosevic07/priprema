89942c09e9d36deefb617e63fb134175ece5a9a7
Aleksa <007milosevicaleksa@gmail.com>
2023-10-24 23:06:42 +0200

    jos uvek prvi

--- najkraciNiz.cpp
+++ najkraciNiz.cpp
@@ -4,12 +4,10 @@ using namespace std;
 int N, M, brojac;
 int resenje = 100000000;
 bool ImaB;
-
 char lavirint[1005][1005];
 
 void DFS(int i, int j)
 {
-    brojac++;
     resenje = min(resenje, brojac);
     if (lavirint[i][j] == 'B')
     {
@@ -26,18 +24,22 @@ void DFS(int i, int j)
 
     if (lavirint[i][j + 1] == '.' or lavirint[i][j+1] == 'B')
     {
+        brojac++;
         DFS(i, j + 1);
     }
     if (lavirint[i + 1][j] == '.' or lavirint[i][j+1] == 'B')
     {
+        brojac++;
         DFS(i + 1, j);
     }
     if (lavirint[i][j - 1] == '.' or lavirint[i][j+1] == 'B')
     {
+        brojac++;
         DFS(i, j - 1);
     }
     if (lavirint[i - 1][j] == '.' or lavirint[i][j+1] == 'B')
     {
+        brojac++;
         DFS(i - 1, j);
     }
 
@@ -63,6 +65,7 @@ int main()
         {
             if (lavirint[i][j] == 'A')
             {
+                brojac = 100000;
                 DFS(i, j);
             }
         }
