e7191e19ec9967b9678750c1fec95cdd1327c687
Aleksa <007milosevicaleksa@gmail.com>
2023-10-26 22:53:42 +0200

    Skakac zadatak dovrsen

--- skakac.cpp
+++ skakac.cpp
@@ -1,54 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-char livadu[1005][1005];
-
-void BFS(int R, int C)
-{
-    queue <int> QR, QC;
-    QR.push(R);
-    QC.push(C);
-    livada[R][C] = '#';
-
-    while(QR.size())
-    {
-        R = QR.front();
-        C = QC.front();
-        if(livada[R+1][C+2] == '.' or livada[R+1][C+2] == 'B')
-        {
-            if(livada[R+1][C+2] == 'B')
-            {
-                return;
-            }
-            QR.push(R+1);
-            QC.push(C+2);
-            livada[R+1][C+2] = '#';
-
-        }
-    }
-
-
-}
-
-
-
+long long M[1005][1005];
+long long X, Y;
 
 int main()
 {
-    memset(livada, '#', sizeof(livada));
-    scanf("%d %d", &N, &M);
-
-    for(int i = 1; i<=N; i++)
+    cin>>X>>Y;
+    M[0][1] = 1;
+    for(int i = 2; i<=X+1; i++)
     {
-        for(int j = 1; j<=M; j++)
+        for(int j = 2; j <= Y+1; j++)
         {
-            livada[i][j] = '.';
+            M[i][j] += M[i-2][j+1];
+            M[i][j] += M[i-2][j-1];
+            M[i][j] += M[i-1][j+2];
+            M[i][j] += M[i-1][j-2];
+
         }
     }
-    livada[N][M] = 'B';
-    BFS(1, 1);
-
-
-
+    cout<<M[X+1][Y+1]<<endl;
     return 0;
 }
